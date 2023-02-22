f6f0c0af92731c24db7bb5b1b03b8ad0ed91c76c
sink666 <er.basilisk@gmail.com>
2023-02-22 15:01:08 +0000

    refactor, restructure

--- src/main/java/net/sink/simplesoups/mixin/ExampleMixin.java
+++ src/main/java/net/sink/simplesoups/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.sink.simplesoups.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.sink.simplesoups.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
